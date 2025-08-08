a06b812114c7b786305e29a32377addc38b62dcf
hanz936 <parma29@seznam.cz>
2025-08-08 17:46:55 +0200

    GitHub Pages routing fix - HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import MyStory from './pages/MyStory';
 import Collaboration from './pages/Collaboration';
